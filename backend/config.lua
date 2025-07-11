41b05c5a2b39be03640937e997fbba1f13b34561
aawwaaa <96404493+aawwaaa@users.noreply.github.com>
2025-07-12 00:46:47 +0800

    OpenPrograms

--- backend/config.lua
+++
@@ -1,6 +0,0 @@
-return {
-    yield_timeout = 0.2,
-    yield_magic_value = { yield_to_process_loop = true },
-
-    loop_yield_timeout = 1,
-}
--- backend/virtual_components/keyboard.lua
+++
@@ -1,8 +0,0 @@
-return function(options)
-    local keyboard = {
-        type = "keyboard",
-        address = "virtual0-keyb-oard-0000-component000",
-    }
-    function keyboard.pushSignal(...) end
-    return keyboard
-end
